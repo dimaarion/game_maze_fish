e26ee69f15a79a6ddd136cbf1c81e23775b71271
Dmitriy <48031182+dimaarion@users.noreply.github.com>
2022-07-10 12:55:48 +0300

    dimaarion/game_maze_fish.git

--- src/App.js
+++ src/App.js
@@ -3,30 +3,35 @@ import Sketch from "react-p5";
 import Matter from "matter-js";
 
 export default class App extends Component {
- 
   World = Matter.World;
-	Bodies = Matter.Bodies;
-	Engine = Matter.Engine;
+  Bodies = Matter.Bodies;
+  Engine = Matter.Engine;
   engine;
   world;
   shape;
-	ground;
+  ground;
   shape_holder = [];
   boxA;
+  d = 0;
   setup = (p5, canvasParentRef) => {
+    this.engine = this.Engine.create();
+    this.world = this.engine.world;
+    // this.Engine.run(this.engine);
+    this.boxA = this.Bodies.rectangle(0, 0, 80, 80);
+    this.boxA.isStatic = true;
+    this.World.add(this.world, this.boxA);
     p5.createCanvas(window.innerWidth, window.innerHeight).parent(
       canvasParentRef
     );
     p5.frameRate(this.fr);
     // use parent to render canvas in this ref (without that p5 render this canvas outside your component)
-    this.engine = this.Engine.create(); 
-    this.world = this.engine.world;
-    this.Engine.run(this.engine);
-    this.boxA = this.Bodies.rectangle(400, 200, 80, 80);
-    this.World.add(this.world,this.boxA);
+    console.log(this.boxA);
   };
-  draw = p5 => {
-    
+  draw = (p5) => {
+    p5.background(100);
+    p5.fill(0);
+    p5.rect(this.boxA.velocity.x, this.boxA.velocity.y, 100, 100);
+    this.Engine.update(this.engine, 16.666, 2);
   };
 
   render() {
